96095642de24a9c7a45fb225d2bccc4348998d6f
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-07-02 20:12:22 +0500

    Working for the functionalities of Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Material(
-        child: Column(
-          children: [
-            const Text("Hello World"),
-            const Text("Hello There"),
-            const Text("I Am Writing This In Column"),
-            MyApp(),
-          ],
-        ),
-      ),
-    ),
+    MyApp(),
   );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Text("I Am Writing This In Stateless Widget");
+    return MaterialApp(
+      home: Material(
+        child: Column(
+          children: [
+            const Text("Hello World"),
+            const Text("Hello There"),
+            const Text("I Am Writing This In Column"),
+            const Text("I Am Writing This In Stateless Widget")
+          ],
+        ),
+      ),
+    );
   }
 }
