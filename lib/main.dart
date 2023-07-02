f5b67bb7e23f3821de379ea5768cbeeba97a3496
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-07-02 20:33:27 +0500

    Lets start building our login page

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Column(
-          children: const [
-            Text("Hello World"),
-            Text("Hello There"),
-            Text("I Am Writing This In Column"),
-            Text("I Am Writing This In Stateless Widget")
-          ],
-        ),
-      ),
+      home:
     );
   }
 }
